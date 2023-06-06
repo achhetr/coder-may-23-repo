dfc44f871d4c7e2968ae55010d36917885bee442
Akash <akash.chhetri1@gmail.com>
2023-06-06 20:55:14 +1000

    week-6(tue): basic/simple data types

---
+++ week-6/tuesday/main.py
@@ -0,0 +1,35 @@
+print("hello world");
+
+
+# input <-> output
+
+# operator operand
+# expression -> one or many operator, we can one or many operands
+# 2 + 2
+
+# statement -> one or many expression
+# (2 + 2)/4
+
+# variable -> pointer to or it contains a value
+# rob_account_balance = 100000000
+# bryan = rob_account_balance - 1000
+
+# primary memory -> RAM
+# secondary memory -> Hard disk
+
+# -
+# if you have installed python
+# if your code has no errors
+# if there are any use input required
+# outcome of the program
+# -
+
+# base 16 -> hexadecimal, decimal (base 10)
+
+# v value
+# reuse the same variable and updates its value
+# address of the value is still same
+
+# v string value
+# reuse the same variable and updates its value
+# address of the value is still changes
