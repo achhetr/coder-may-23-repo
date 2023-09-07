4a9a70b47bcda4df92f9582129286d75a471bf67
Akash <akash.chhetri1@gmail.com>
2023-09-07 20:21:44 +1000

    third problem solved

--- term-2/week-567/thurs-sat/main.py
+++ term-2/week-567/thurs-sat/main.py
@@ -5,6 +5,7 @@ from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from config import app_config
+import sqlalchemy as sa
 
 bcrypt = Bcrypt()
 db = SQLAlchemy()
@@ -37,5 +38,16 @@ def init_app():
 
     for controller in registered_controllers:
         app.register_blueprint(controller)
+
+    db_engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    db_inspector = sa.inspect(db_engine)
+
+    if not db_inspector.has_table("users"):
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+            app.logger.info("New database initialized!")
+    else:
+        app.logger.info("Database already exists!")
 
     return app
